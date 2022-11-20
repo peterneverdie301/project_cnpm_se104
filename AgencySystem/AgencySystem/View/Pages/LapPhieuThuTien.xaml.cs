6a4e7d5920a6c2bff7f09767da1571fb74c83bfd
peterneverdie301 <lahuy3012002@gmail.com>
2022-11-20 20:30:14 +0700

    - add

---
+++ AgencySystem/AgencySystem/View/Pages/LapPhieuThuTien.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace AgencySystem.View.Pages;
+
+public partial class LapPhieuThuTien : Page
+{
+    public LapPhieuThuTien()
+    {
+        InitializeComponent();
+    }
+}

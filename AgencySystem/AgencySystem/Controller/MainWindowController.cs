8184b2fc4fa8a1e912bd5d6b6a014a66cac26d79
vanlinh1602 <vanlinh14121@gmail.com>
2022-12-02 22:53:44 +0700

    remove home page defaut and add router for main windown

---
+++ AgencySystem/AgencySystem/Controller/MainWindowController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgencySystem.Controller
+{
+    public class MainWindowController
+    {
+        public void Close()
+        {
+            this.Close();
+        }
+    }
+}

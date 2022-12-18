0baac1aef935dbf1951abc8eea5a49ef17c744b7
James2109 <20520292@gm.uit.edu.vn>
2022-12-18 20:54:37 +0700

    ok

--- AgencySystem/AgencySystem/View/Pages/AddingExportSlip.xaml.cs
+++ AgencySystem/AgencySystem/View/Pages/AddingExportSlip.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using DataModels.Models;
+using DataModels.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,20 +20,49 @@ namespace AgencySystem.View.Pages;
 
 public partial class AddingExportSlip : Page
 {
+    private Firestore firestore = new Firestore();
+    List<object> agences;
+    List<object> products;
+    List<object> units;
     public AddingExportSlip()
     {
         InitializeComponent();
-        List<User> items = new List<User>();
-        items.Add(new User() { Product = "ca com", Quantity = 20,  Unit= "kg" });
-        lvUsers.ItemsSource = items;                
+        setUp();               
+    }
+    private async void setUp()
+    {
+        agences = new List<object>();
+        agences = await firestore.GetAllDocument(Utils.Collection.Agency.ToString());
+        foreach (Agency agency in agences)
+        {
+            if (agency.AgencyName != null)
+            {
+                cbxAgency.Items.Add(agency.AgencyName);
+            }
+        }
+        products = new List<object>();
+        products = await firestore.GetAllDocument(Utils.Collection.Items.ToString());
+        foreach (Item item in products)
+        {
+            if (item.ItemsName != null)
+            {
+                CbProduct.Items.Add(item.ItemsName);
+            }
+        }
+        units = new List<object>();
+        units = await firestore.GetAllDocument(Utils.Collection.Units.ToString());
+        foreach (Unit unit in units)
+        {
+            if (unit.Value != null)
+            {
+                CbUnit.Items.Add(unit.Value);
+            }
+        }
     }
 
-    public class User
+    private void TbQuantity_PreviewTextInput(object sender, TextCompositionEventArgs e)
     {
-        public string Product { get; set; }
-
-        public int Quantity { get; set; }
-
-        public string Unit { get; set; }
+        Regex regex = new Regex("[^0-9]+");
+        e.Handled = regex.IsMatch(e.Text);
     }
 }

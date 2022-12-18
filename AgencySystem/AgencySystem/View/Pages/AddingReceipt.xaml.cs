71b3026021d005a973ae79cb5c72ebb5f21ff97f
vanlinh1602 <vanlinh14121@gmail.com>
2022-12-18 15:48:20 +0700

    set up func

--- AgencySystem/AgencySystem/View/Pages/AddingReceipt.xaml.cs
+++ AgencySystem/AgencySystem/View/Pages/AddingReceipt.xaml.cs
@@ -1,11 +1,36 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using DataModels.Models;
+using DataModels.Services;
 
 namespace AgencySystem.View.Pages;
 
 public partial class AddingReceipt : Page
 {
+    private Firestore firestore = new Firestore();
+    List<object> agences;
     public AddingReceipt()
     {
         InitializeComponent();
+        setUp();
+    }
+    private async void setUp()
+    {
+        agences = new List<object>();
+        agences = await firestore.GetAllDocument(Utils.Collection.Agency.ToString());
+        foreach (Agency agency in agences)
+        {
+            if (agency.AgencyName != null)
+            {
+                cbxAgency.Items.Add(agency.AgencyName);
+            }
+        }
+
+    }
+
+    private void Handle_Add(object sender, RoutedEventArgs e)
+    {
+
     }
 }

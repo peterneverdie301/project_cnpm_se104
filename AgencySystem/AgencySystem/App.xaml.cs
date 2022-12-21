3ad41feac1a297bfabefe0523e631c4bb286c15b
vanlinh1602 <vanlinh14121@gmail.com>
2022-12-21 23:14:50 +0700

    update login screen

--- AgencySystem/AgencySystem/App.xaml.cs
+++ AgencySystem/AgencySystem/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -15,11 +16,29 @@ namespace AgencySystem
     /// </summary>
     public partial class App : Application
     {
+        MainWindow main;
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            startMainWindow();
+        }
 
-            MainWindow main = new MainWindow();
+        void startMainWindow()
+        {
+            main = new MainWindow();
+            main.Hide();
+            startLoginWindow();
+        }
+
+        void startLoginWindow()
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Closed += LoginWindow_Closed;
+            loginWindow.Show();
+        }
+
+        private void LoginWindow_Closed(object? sender, EventArgs e)
+        {
             main.Show();
         }
     }
